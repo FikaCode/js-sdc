a608928e73be8950464eca3933ca02f297fba02e
FikaCode <wangfashuai1992@outlook.com>
2020-10-28 11:03:33 +0800

    message support js object

--- js-sdc.js
+++ js-sdc.js
@@ -1,54 +1,69 @@
 /**
- * js-sdc 
- * a simple html same domain communication by localStorage storage event
+ * sdc module
  */
-//prifix random num
-var SDCFingerPrinter = "SDC_prefix_rdm_7591_";
+layui.define([], function (exports) {
 
-//store channle and hanle
-var SDCHandleMap = [];
+    /**
+    * js-sdc 
+    * a simple html same domain communication by localStorage storage event
+    */
+    //prifix random num
+    var SDCFingerPrinter = "SDC_prefix_rdm_7591_";
 
-/**
- * init addEventListener 'storage' event
- */
-function init() {
-    window.addEventListener("storage", function (e) {
-        for (kh of SDCHandleMap) {
-            if (kh.key === e.key) {
-                kh.handle(e.newValue);
-                window.localStorage.setItem(kh.key, SDCFingerPrinter);
+    //store channle and hanle
+    var SDCHandleMap = [];
+
+    /**
+     * init addEventListener 'storage' event
+     */
+    function init() {
+        window.addEventListener("storage", function (e) {
+            for (kh of SDCHandleMap) {
+                if (kh.key === e.key) {
+                    if (e.newValue === SDCFingerPrinter) {
+                        continue;
+                    }
+                    kh.handle(JSON.parse(e.newValue));
+                    window.localStorage.setItem(kh.key, SDCFingerPrinter);
+                }
+            }
+
+        });
+    }
+
+    var sdcHandle = {
+        /**
+        * minitor channel message
+        * @param  channel  
+        * @param  handle 
+        */
+        onSDCMessage: function (channel, handle) {
+            if (channel === '' || typeof handle !== 'function') {
+                console.error('SDCMessage failure: channel is "" or handle is not a function');
+                return;
+            }
+            window.localStorage.setItem(SDCFingerPrinter + channel, SDCFingerPrinter);
+            SDCHandleMap.push({ key: SDCFingerPrinter + channel, handle: handle });
+        },
+
+        /**
+         * send messsage to channle, if  onSDCMessage and sendSDCMessage is on same Page, isSamePage need to be true
+         * @param  channel 
+         * @param  message 
+         */
+        sendSDCMessage: function (channel, message, isSamePage) {
+            window.localStorage.setItem(SDCFingerPrinter + channel, JSON.stringify(message));
+            if (isSamePage === true) {
+                var event = new Event("storage");
+                event.key = SDCFingerPrinter + channel;
+                event.newValue = JSON.stringify(message);
+                window.dispatchEvent(event);
             }
         }
-
-    });
-}
-
-/**
- * minitor channel message
- * @param  channel  
- * @param  handle 
- */
-function onSDCMessage(channel, handle) {
-    if (channel === '' || typeof handle !== 'function') {
-        console.error('SDCMessage failure: channel is "" or handle is not a function');
-        return;
     }
-    window.localStorage.setItem(SDCFingerPrinter + channel, SDCFingerPrinter);
-    SDCHandleMap.push({ key: SDCFingerPrinter + channel, handle: handle });
-}
 
-/**
- * send messsage to channle，if onSDCMessage and sendSDCMessage is on same Page, isSamePage need to be true
- * @param  channel 
- * @param  message 
- */
-function sendSDCMessage(channel, message, isSamePage) {
-    window.localStorage.setItem(SDCFingerPrinter + channel, message);
-    if (isSamePage === true) {
-        var event = new Event("storage");
-        event.key = SDCFingerPrinter + channel;
-        event.newValue = message
-        window.dispatchEvent(event);
-    }
-}
-init();
+    init();
+
+    exports('sdc', sdcHandle);
+
+});
